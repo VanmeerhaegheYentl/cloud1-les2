fec4efc81c6e61711727b596ab6ede4ca4ac3532
VanmeerhaegheYentl <yentl.vanmeerhaeghe@studend.howest.be>
2018-10-04 10:26:06 +0200

    fuctionap added

---
+++ Cloud1-FunctionApp1/Function1.cs
@@ -0,0 +1,55 @@
+
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Cloud1FunctionApp1
+{
+    public static class Function1
+    {
+        [FunctionName("HelloWorld")]
+        public static async Task<IActionResult> HelloWorld([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "hello/{name}/{age}")]HttpRequest req, string name, int age, ILogger log)
+        {
+            //log.LogInformation("C# HTTP trigger function processed a request.");
+
+            //string name = req.Query["name"];
+
+            //string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            //dynamic data = JsonConvert.DeserializeObject(requestBody);
+            //name = name ?? data?.name;
+
+            //return name != null
+            //    ? (ActionResult)new OkObjectResult($"Hello, {name}")
+            //    : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+
+            string result = "hello " + name + "\nyou are " + age + " years old";
+            return new OkObjectResult(result);
+        }
+
+        [FunctionName("rekenmachine")]
+        public static async Task<IActionResult> rekenmachine([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "som/{nr1}/{nr2}")]HttpRequest req, int nr1, int nr2, ILogger log)
+        {
+            //log.LogInformation("C# HTTP trigger function processed a request.");
+
+            //string name = req.Query["name"];
+
+            //string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            //dynamic data = JsonConvert.DeserializeObject(requestBody);
+            //name = name ?? data?.name;
+
+            //return name != null
+            //    ? (ActionResult)new OkObjectResult($"Hello, {name}")
+            //    : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+
+            string result = nr1 + " + " + nr2 + " = " + (nr1 + nr2);
+            return new OkObjectResult(result);
+        }
+    }
+}
